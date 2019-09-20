a1a58ddfb8a80b3b8a39e17e46075732371a8c98
Alex Turner <alex.turner@safetyculture.io>
2019-09-20 16:32:20 +1000

    remove testing comments

--- spotify-tracker.py
+++ spotify-tracker.py
@@ -19,37 +19,20 @@ token = util.prompt_for_user_token(username, scope)
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
         track = item['track']
-        # print "   %d %32.32s %s" % (i, track['artists'][0]['name'], track['name'])
-        # print(i, track['artists'][0]['name'], track['name'])
         track_text = '{id} - {artist} - {album} - {title}'.format(
             id=track['id'], artist=track['artists'][0]['name'], album=track['album']['name'], title=track['name']
         )
         print(track_text)
-
-#         insert_sql = 'INSERT INTO discover_weekly_tracks (track_id, track_artist, track_album, track_title) VALUES ("{id}", "{artist}", "{album}", "{title}")'.format(
-#             id=track['id'], artist=track['artists'][0]['name'], album=track['album']['name'], title=track['name'])
-
-
-# cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",
-# ...      (100, "abc'def"))
-
-#         print(insert_sql)
-
-        # cur.execute(insert_sql)
 
         cur.execute('INSERT INTO discover_weekly_tracks (track_id, track_artist, track_album, track_title) VALUES (%s,%s,%s,%s)', 
         (track['id'], track['artists'][0]['name'], track['album']['name'], track['name']))
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    # playlists = sp.user_playlists(username)
     playlists = sp.current_user_playlists()
     for playlist in playlists['items']:
-        # print(playlist['name'])
+
         if playlist['name'] == 'Discover Weekly':
-            # print(playlist['name'])
-            # print(playlist)
-            # print('  total tracks', playlist['tracks']['total'])
             results = sp.user_playlist(username, playlist['id'],
                 fields="tracks,next")
             tracks = results['tracks']
